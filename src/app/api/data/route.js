f28a6ba25bc400ad852e95ae4cd98a87ffc66494
nitish0104 <nitishdalvi1@gmail.com>
2023-08-26 10:45:16 +0530

    Post APi

--- src/app/api/data/route.js
+++ src/app/api/data/route.js
@@ -6,5 +6,13 @@ export function GET(req, res) {
 
 	return NextResponse.json(data)
 }
+export async function POST(req, res) {
+	let payload = await req.json()
+	console.log(payload)
+
+	return NextResponse.json({ result: payload })
+
+
+}
 
 
